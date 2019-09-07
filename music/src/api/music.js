a17a3fce9db5f1bd67ae612c82e4354f64220934
mayingjie <1186806862@qq.com>
2019-09-07 15:22:38 +0800

    增删改查

--- music/src/api/music.js
+++ music/src/api/music.js
@@ -1,7 +1,11 @@
 import request from '@/utils/request'
 
 let music = {
-    querymusic:data => request.get('/api/querymusic',data)  
+    querymusic:data => request.get('/api/querymusic',data),
+    addmusic:data => request.post('/api/addmusic',data),
+    upload: data => request.post('/api/upload',data),
+    delete:data => request.get('/api/deletemusic',data),
+    updatemusic:data => request.post('/api/updatemusic',data)
 }
 
 export default music
--- music/src/views/music/index.vue
+++ music/src/views/music/index.vue
@@ -10,36 +10,73 @@
             <el-container>
             <el-aside width="200px">Aside</el-aside>
             <el-main>
-                <el-table
-                    :data="tableData"
-                    style="width: 100%">
-                    <el-table-column
-                    label="Date"
-                    prop="date">
+                <el-button @click="open">添加</el-button>
+                <!-- 添加音乐表格 -->
+                <el-table :data="MusicList" style="width: 100%">
+                    <el-table-column label="序号" type='index'></el-table-column>
+                    
+                    <el-table-column label="歌名" prop="music_name"> </el-table-column>
+                   
+                    <el-table-column label="图片" >
+                    <!-- 作用域插槽 -->
+                     <template slot-scope="scope">
+                            <img :src="scope.row.pic" alt="" class="m-img">
+                        </template>
                     </el-table-column>
-                    <el-table-column
-                    label="Name"
-                    prop="name">
+                      <el-table-column label="歌手" prop="singer_name">
+                      </el-table-column>
+                      <el-table-column label="是否上架" prop="">
+                        <template slot-scope="scope">
+                            <span>{{scope.row.isup ? '是' :'否'}}</span>
+                        </template>
                     </el-table-column>
-                    <el-table-column
-                    align="right">
-                    <!-- <template slot="header" slot-scope="scope">
-                        <el-input
-                        v-model="search"
-                        size="mini"
-                        placeholder="输入关键字搜索"/>
-                    </template> -->
-                    <template slot-scope="scope">
-                        <el-button
-                        size="mini"
-                        @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                        <el-button
-                        size="mini"
-                        type="danger"
-                        @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-                    </template>
+                    <el-table-column label="操作" align="right">
+                        <template slot-scope="scope">
+                            <el-button size="mini" @click="handleEdit(scope.$index, scope.row, scope.row.id)">编辑</el-button>
+                            <el-button size="mini"  type="danger" @click="handleDelete(scope.$index, scope.row,scope.row.id)">删除</el-button>
+                        </template>
                     </el-table-column>
                 </el-table>
+                  <!-- 分页器 -->
+                     <el-pagination
+                        background
+                        layout="prev, pager, next"
+                        :total="total"
+                        :page-size="limit"
+                        @current-change="pageChange"
+                        >
+                    </el-pagination>
+                <!-- 添加弹框 -->
+                <el-dialog title="提示" :visible.sync="dialogVisible" width="50%">
+                    <span>
+                        <el-form :model="ruleForm" status-icon  ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                            <el-form-item label="歌名" prop="music_name">
+                                <el-input type="text" v-model="ruleForm.music_name" autocomplete="off"></el-input>
+                            </el-form-item>
+                            <el-form-item label="歌手" prop="singer_name">
+                                <el-input type="text" v-model="ruleForm.singer_name" autocomplete="off"></el-input>
+                            </el-form-item>
+                            <el-form-item label="上传图片" prop="age">
+                               <input type="file" @change="upload">
+                               <img  :src="ruleForm.pic" class="m-img"/>
+                            </el-form-item>
+                            <el-form-item label="是否上架" prop="pass">
+                                <el-radio-group v-model="ruleForm.isup">
+                                    <el-radio label="1">是</el-radio>
+                                    <el-radio label="0">否</el-radio>
+                                </el-radio-group>
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button type="primary" @click="submitForm('ruleForm')">{{editId ? "添加" : "完成"}}</el-button>
+                                <el-button @click="resetForm('ruleForm')">重置</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </span>
+                    <!-- <span slot="footer" class="dialog-footer">
+                        <el-button @click="dialogVisible = false">取 消</el-button>
+                        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                    </span> -->
+                </el-dialog>
             </el-main>
             </el-container>
     </el-container>
@@ -50,30 +87,33 @@ import {mapState,mapMutations} from 'vuex'
 export default {
     data(){
         return{
-             tableData: [{
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
-                }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄'
-                }, {
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄'
-                }],
+             MusicList: [],
+                limit:3,//每页展示的条数
+                pagenum:1,//展示页数
+                total:0,
+                dialogVisible: false,//控制弹框的显示
+                editId:'',
+                ruleForm:{
+                    music_name:'',
+                    singer_name:'',
+                    pic:'',
+                    isup:1
+                }
+
         }
     },
     methods:{
         ...mapMutations(['setName']),
+        //点击跳到登录页面
         denglu(){
             this.$router.push('/login')
         },
+        //点击分页器
+        pageChange(val){
+             this.pagenum = val;
+             this.getMusicList()
+        },
+        //点击退出页面
         quit(){
             this.$confirm('是否退出', '提示', {
                 confirmButtonText: '确定',
@@ -95,11 +135,91 @@ export default {
                 });          
             });
         },
-        handleEdit(index, row) {
-        console.log(index, row);
+        //点击编辑
+        handleEdit(index, row,id) {
+        console.log(id);
+        this.dialogVisible = true
+        this.editId = id;
+        let editObj = this.MusicList.find(item => item.id === id);
+        this.ruleForm.music_name = editObj.music_name;
+        this.ruleForm.singer_name = editObj.singer_name;
+        this.ruleForm.isup = editObj.isup;
+        this.ruleForm.pic = editObj.pic;
+        this.$api.music.updatemusic(Object.assign({},this.ruleForm,{id:this.editId})).then(res => {
+            if(res.data.code === 1) {
+                this.getMusicList()
+            }
+        })
+            if(id){
+                
+            }
+
+          this.$nextTick( () => {
+                     this.$message({
+                        message:'编辑完成',
+                        type:'success'
+                    })
+            })
+         
         },
-        handleDelete(index, row) {
+        //点击删除
+        handleDelete(index, row,id) {
             console.log(index, row);
+            this.$api.music.delete({id}).then( res => {
+                if(res.data.code === 1) {
+                    this.$message({
+                        message:'删除成功',
+                        type:'success'
+                    })
+                    this.MusicList.length <= 1 && this.pagenum > 1 ? --this.pagenum : this.pagenum;
+                    this. getMusicList()
+                }
+            })
+        },
+        //点击添加列表项
+        open(){
+            this.dialogVisible = true
+        },
+        //请求音乐列表
+        getMusicList(){
+            this.$api.music.querymusic({pagenum:this.pagenum,limit:this.limit}).then(res => {
+                if( res.data.code == 1){
+                    this.MusicList= res.data.data
+                    this.total = res.data.total
+                }
+            })
+        },
+        //点击添加将音乐列表添加到列表项中
+        submitForm(){
+            // this.dialogVisible =false
+            this.$api.music.addmusic(this.ruleForm).then( res => {
+                if(res.data.code == 1) {
+                    this.getMusicList();
+                    this.resetForm()
+                    this.dialogVisible = false;
+                }
+            })
+        },
+        //点击重置按钮的时候将添加弹框里的内容清空
+        resetForm(){
+            this.ruleForm. music_name ='';
+            this.ruleForm. singer_name ='';
+            this.ruleForm. pic ='';
+            this.ruleForm. isup =''
+        },
+        //上传图片
+        upload(e){
+            let file = e.target.files[0];
+            //1、新建formdata的实例
+            let formdata = new FormData();
+            //2.添加参数
+            formdata.append('file',file)
+            //3.请求上传图片的接口
+            this.$api.music.upload(formdata).then( res => {
+                if( res.data.code === 1 ) {
+                    this.ruleForm.pic = 'http://localhost:3000' + res.data.url
+                }
+            })
         }
     },
     created(){
@@ -107,9 +227,11 @@ export default {
             console.log(res.data.data.username)
             if(res.data.code == 1){
                  this.setName(res.data.data.username)
-            }
-            
+            } 
         })
+         //请求音乐列表
+        this.getMusicList()
+        
     },
     computed:{
         ...mapState({
@@ -161,5 +283,9 @@ export default {
   }
   .m-span{
       margin-right: 20px;
+  }
+  .m-img{
+      width: 80px;
+      height: 80px;
   }
   </style>
